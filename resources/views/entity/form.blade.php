e2e74e33f592ef7448417085a8fe1d9fa778f774
daruiza <daruiza@gmail.com>
2019-01-05 11:11:58 -0500

    sutin change

--- resources/views/entity/form.blade.php
+++ resources/views/entity/form.blade.php
@@ -1,1 +1,13 @@
-form.blade.php
+<div class="form-group row">
+	{!! Form::label('name',__('messages.Name'),['class'=>'col-sm-4 col-form-label text-md-right']) !!}
+	<div class="col-md-8">
+		@if($errors->has('name'))
+			{!! Form::text('name',null, ['class'=>'form-control is-invalid']) !!}
+			<span class="invalid-feedback">
+                <strong>{{ $errors->first('name') }}</strong>
+            </span>
+		@else
+			{!! Form::text('name',null, ['class'=>'form-control']) !!}
+		@endif	
+	</div>
+</div>
